6763fd12c630446db452a5ff0153c5e42a0f2594
Liam Hughes <liam@liamhughes.com>
2020-07-13 09:21:37 +1000

    Stub the CLI

---
+++ FirstThingsCLI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace FirstThingsCLI
+{
+    class Program
+    {
+        const string FILE_PATH = "/Users/liam/Downloads/TickTick.csv";
+
+        static void Main(string[] args)
+        {
+            var fileSystem = new System.IO.Abstractions.FileSystem();
+
+            var taskLoader = new FirstThingsLib.FileTaskLoader(fileSystem, FILE_PATH);
+
+            var tasks = taskLoader.LoadTasks();
+
+            var tasksToOutput = tasks
+                                    .Where(t => t.ListName == "Personal - New")
+                                    .Where(t => t.Status == 0)
+                                    .Where(t => t.StartDate <= DateTime.Now);
+
+            foreach(var task in tasksToOutput.OrderBy(t => t.Order))
+            {
+                Console.WriteLine($"{task.Title}");
+            }
+        }
+    }
+}
--- FirstThingsLib/Task.cs
+++ FirstThingsLib/Task.cs
@@ -1,13 +1,20 @@
 using System;
+using CsvHelper.Configuration.Attributes;
 
 namespace FirstThingsLib
 {
     public class Task
     {
+        [Name("List Name")]
         public string ListName { get; set; }
-        public long Order { get; set; }
-        public DateTime StartDate { get; set; }
+
+        public double Order { get; set; }
+
+        [Name("Start Date")]
+        public DateTime? StartDate { get; set; }
+
         public int Status { get; set; }
+
         public string Title { get; set; }
     }
 }
